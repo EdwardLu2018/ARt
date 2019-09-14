f8cf82d49dd665953d5366bc38c05427f02f8ea2
Sophia Lau <slau2022@gmail.com>
2019-09-14 14:56:13 -0400

    Unusued functions removed

--- getMap.py
+++ getMap.py
@@ -8,21 +8,6 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-
-def make_soup(url):
-    http = urllib3.PoolManager()
-    r = http.request("GET", url)
-    return BeautifulSoup(r.data,'lxml')
-
-
-def spell_correct (artist):
-	artist = artist.strip().replace (" ", "%20")
-	url = "https://www.wikiart.org/en/Search/" + artist + "#!#filterName:Style_post-impressionism,resultType:masonry"
-	soup = make_soup (url)
-	divs = soup.find_all('a')
-	for div in divs:
-		print( (div.text))
-
 def getMap(place):
 	#takes in a list of places and gives out image places in io.bytes
 	api_key = "AIzaSyA77bNJRtXVDm_SdTUihAixskEhUK8c98I"
